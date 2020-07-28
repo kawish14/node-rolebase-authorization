51fb2a92524dd0091ba6ef0ae82d58e830f31751
Kawish Abbas <kawish.mehdi14@gmail.com>
2020-07-28 11:01:20 +0500

    first commit

---
+++ server.js
@@ -0,0 +1,67 @@
+﻿require('rootpath')();
+const express = require('express');
+var path = require('path');
+const cors = require('cors');
+const app = express();
+const bodyParser = require('body-parser');
+const errorHandler = require('_helpers/error-handler');
+var sql = require("mssql");
+
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.json());
+//app.use(express.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+ const pool = new sql.ConnectionPool({
+        user: 'sa',
+        password: 'Gis@123',
+        server: 'KHI-ENG-KABBAS', 
+        database: 'Karachi',
+        options: {
+        trustedConnection: true
+        },
+        port: 1433,
+    });
+
+ async function getUserData(req,res){
+   
+    // config for your database
+
+      await pool.connect().then(function(err){
+    
+        if (err) console.log(err);
+
+        // create Request object
+        var request = new sql.Request(pool);
+           
+        // query to the database and get the records
+        request.query("SELECT * FROM Users", function (err, result) {
+            
+            if (err) console.log(err)
+         
+         // send records as a response        
+          res.send(result.recordset)
+          console.log(result.recordset)
+            pool.close()
+
+            
+        });
+    });
+
+  
+}
+app.get('/userdata', getUserData)
+// api routes
+app.use('/users', require('./users/users.controller'));
+
+// global error handler
+app.use(errorHandler);
+
+// start server
+const port = process.env.NODE_ENV === 'production' ? 80 : 2000;
+const server = app.listen(port, function () {
+    console.log('Server listening on port ' + port);
+});
---
+++ users/user.service.js
@@ -0,0 +1,52 @@
+﻿const config = require('config.json');
+const jwt = require('jsonwebtoken');
+const Role = require('_helpers/role');
+
+
+// users hardcoded for simplicity, store in a db for production applications
+const users = [
+    { id: 1, username: 'kawish', password: 'kawish123', firstName: 'Kawish', lastName: 'Abbas', role: Role.Admin },
+    { id: 2, username: 'mujtaba', password: 'mujtaba123', firstName: 'Mujtaba', lastName: 'Zaidi', role: Role.User },
+    { id: 3, username: 'arsalan', password: 'arsalan123', firstName: 'Arsalan', lastName: 'Gay', role: Role.SouthDEV }
+];
+
+module.exports = {
+    authenticate,
+    getAll,
+    getSouthDev,
+    getById,
+  
+};
+
+async function authenticate({ username, password }) {
+    const user = users.find(u => u.username === username && u.password === password);
+    if (user) {
+        const token = jwt.sign({ sub: user.id, role: user.role }, config.secret);
+        const { password, ...userWithoutPassword } = user;
+        return {
+            ...userWithoutPassword,
+            token
+        };
+    }
+}
+
+async function getAll() {
+    return users.map(u => {
+        const { password, ...userWithoutPassword } = u;
+        return userWithoutPassword;
+    });
+}
+
+async function getSouthDev() {
+    return users.map(u => {
+        const { password, ...userWithoutPassword } = u;
+        return userWithoutPassword;
+    });
+}
+
+async function getById(id) {
+    const user = users.find(u => u.id === parseInt(id));
+    if (!user) return;
+    const { password, ...userWithoutPassword } = user;
+    return userWithoutPassword;
+}
